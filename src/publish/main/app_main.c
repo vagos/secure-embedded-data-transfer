2aaa2223da8c5ca62d2603ee294a72e8fc3ad5e0
chensy618 <146075999+chensy618@users.noreply.github.com>
2023-10-10 18:12:26 +0200

    Add ADC code

--- src/publish/main/app_main.c
+++ src/publish/main/app_main.c
@@ -26,12 +26,19 @@
 #include "esp_ota_ops.h"
 #include <sys/param.h>
 #include "ring_buffer.h"
+#include "continuous_read.h"
 
+#if CONFIG_IDF_TARGET_ESP32
+static adc_channel_t channel[1] = {ADC_CHANNEL_6};
+#else
+static adc_channel_t channel[2] = {ADC_CHANNEL_2, ADC_CHANNEL_3};
+#endif
 
 static const char *TAG = "SEDT";
 static int CLIENT_ID;
 static const int MQTT_RING_SIZE = 10;
 static RING_BUFFER_T *mqtt_ring_buffer = NULL;
+static adc_continuous_handle_t adc_handle = NULL;
 
 #undef CONFIG_BROKER_URL
 #define CONFIG_BROKER_URL "mqtt://broker.hivemq.com"
@@ -46,6 +53,27 @@ extern const uint8_t mqtt_eclipseprojects_io_pem_start[]   asm("_binary_mqtt_pem
 #endif
 extern const uint8_t mqtt_eclipseprojects_io_pem_end[]   asm("_binary_mqtt_pem_end");
 
+/*
+ * ADC function
+ * 
+ */
+#if 0
+static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle, const adc_continuous_evt_data_t *edata, void *user_data)
+{
+    BaseType_t mustYield = pdFALSE;
+    //Notify that ADC continuous driver has done enough number of conversions
+    vTaskNotifyGiveFromISR(adc_task_handle, &mustYield);
+
+    return (mustYield == pdTRUE);
+}
+#endif
+static void adc_init()
+{
+    continuous_adc_init(channel, sizeof(channel) / sizeof(adc_channel_t), &adc_handle);
+    ESP_ERROR_CHECK(adc_continuous_start(adc_handle));
+}
+
+/*----------------------------------------------------------------------------------------------------------*/
 //
 // Note: this function is for testing purposes only publishing part of the active partition
 //       (to be checked against the original binary)
@@ -61,7 +89,6 @@ static void send_binary(esp_mqtt_client_handle_t client)
     int msg_id = esp_mqtt_client_publish(client, "/topic/binary", binary_address, binary_size, 0, 0);
     ESP_LOGI(TAG, "binary sent with msg_id=%d", msg_id);
 }
-
 /*
  * Event handler registered to receive MQTT events
  * This function is called by the MQTT client event loop.
@@ -71,41 +98,48 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32, base, event_id);
     esp_mqtt_event_handle_t event = event_data;
     esp_mqtt_client_handle_t client = event->client;
-    switch ((esp_mqtt_event_id_t)event_id) {
-    case MQTT_EVENT_CONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        break;
-    case MQTT_EVENT_DISCONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-        break;
-    case MQTT_EVENT_PUBLISHED:
-        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
-        break;
-    case MQTT_EVENT_DATA:
-        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-        printf("DATA=%.*s\r\n", event->data_len, event->data);
-        if (strncmp(event->data, "send binary please", event->data_len) == 0) {
-            ESP_LOGI(TAG, "Sending the binary");
-            send_binary(client);
-        }
-        break;
-    case MQTT_EVENT_ERROR:
-        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
-            ESP_LOGI(TAG, "Last error code reported from esp-tls: 0x%x", event->error_handle->esp_tls_last_esp_err);
-            ESP_LOGI(TAG, "Last tls stack error number: 0x%x", event->error_handle->esp_tls_stack_err);
-            ESP_LOGI(TAG, "Last captured errno : %d (%s)",  event->error_handle->esp_transport_sock_errno,
-                     strerror(event->error_handle->esp_transport_sock_errno));
-        } else if (event->error_handle->error_type == MQTT_ERROR_TYPE_CONNECTION_REFUSED) {
-            ESP_LOGI(TAG, "Connection refused error: 0x%x", event->error_handle->connect_return_code);
-        } else {
-            ESP_LOGW(TAG, "Unknown error type: 0x%x", event->error_handle->error_type);
-        }
-        break;
-    default:
-        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
-        break;
+    switch ((esp_mqtt_event_id_t)event_id) 
+    {
+        case MQTT_EVENT_CONNECTED:
+            ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+            break;
+        case MQTT_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+            break;
+        case MQTT_EVENT_PUBLISHED:
+            ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+            break;
+        case MQTT_EVENT_DATA:
+            ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+            printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+            printf("DATA=%.*s\r\n", event->data_len, event->data);
+            if (strncmp(event->data, "send binary please", event->data_len) == 0) 
+            {
+                ESP_LOGI(TAG, "Sending the binary");
+                send_binary(client);
+            }
+            break;
+        case MQTT_EVENT_ERROR:
+            ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+            if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) 
+            {
+                ESP_LOGI(TAG, "Last error code reported from esp-tls: 0x%x", event->error_handle->esp_tls_last_esp_err);
+                ESP_LOGI(TAG, "Last tls stack error number: 0x%x", event->error_handle->esp_tls_stack_err);
+                ESP_LOGI(TAG, "Last captured errno : %d (%s)",  event->error_handle->esp_transport_sock_errno,
+                        strerror(event->error_handle->esp_transport_sock_errno));
+            } 
+            else if (event->error_handle->error_type == MQTT_ERROR_TYPE_CONNECTION_REFUSED) 
+            {
+                ESP_LOGI(TAG, "Connection refused error: 0x%x", event->error_handle->connect_return_code);
+            } 
+            else 
+            {
+                ESP_LOGW(TAG, "Unknown error type: 0x%x", event->error_handle->error_type);
+            }
+            break;
+        default:
+            ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+            break;
     }
 }
 
@@ -139,9 +173,9 @@ static void mqtt_app_start(void *arg)
 			{
                 //&mqtt_msg[i]
 				int msg_id = esp_mqtt_client_publish(client, TOPIC_PUBLISH, (char *)mqtt_msg_buffer[i].data, 0, 0, 0);
-                ESP_LOGI(TAG, "sent publish successful, msg_id=%d client=%d", msg_id, CLIENT_ID);
+                ESP_LOGI(TAG, "sent publish successful, msg_id=%d client=%d,data:%s", msg_id, CLIENT_ID,mqtt_msg_buffer[i].data);
 			}  
-            ESP_LOGI(TAG, "sent publish successful\r\n");
+            //ESP_LOGI(TAG, "sent publish successful\r\n");
 		}
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
@@ -151,9 +185,40 @@ static void data_app_start(void *arg)
     MIC_DATA_T mic_data;
     char buf[] = "23"; 
     ESP_LOGI(TAG, "data_app_start\r\n");
+    uint32_t ret_num = 0;
+    esp_err_t ret;
+    uint8_t result[EXAMPLE_READ_LEN] = {0};
+    memset(result, 0xcc, EXAMPLE_READ_LEN);
+    char unit[] = EXAMPLE_ADC_UNIT_STR(EXAMPLE_ADC_UNIT);
+    adc_init();
 
     while(true)
     {
+        ret = adc_continuous_read(adc_handle, result, EXAMPLE_READ_LEN, &ret_num, 0);
+        if (ret == ESP_OK)
+        {
+            //ESP_LOGI("TASK", "ret is %x, ret_num is %"PRIu32" bytes", ret, ret_num);
+            for (int i = 0; i < ret_num; i += SOC_ADC_DIGI_RESULT_BYTES) 
+            {
+                adc_digi_output_data_t *p = (void*)&result[i];
+                uint32_t chan_num = EXAMPLE_ADC_GET_CHANNEL(p);
+                uint32_t data = EXAMPLE_ADC_GET_DATA(p);
+                /* Check the channel number validation, the data is invalid if the channel num exceed the maximum channel */
+                if (chan_num < SOC_ADC_CHANNEL_NUM(EXAMPLE_ADC_UNIT))
+                {
+                    ESP_LOGI(TAG, "Unit: %s, Channel: %"PRIu32", Value: %"PRIx32, unit, chan_num, data);
+                } 
+                else 
+                {
+                    ESP_LOGW(TAG, "Invalid data [%s_%"PRIu32"_%"PRIx32"]", unit, chan_num, data);
+                }
+            }
+        } 
+        else if (ret == ESP_ERR_TIMEOUT) 
+        {
+            //We try to read `EXAMPLE_READ_LEN` until API returns timeout, which means there's no available data
+            ESP_LOGW(TAG, "ADC Timeout\r\n");
+        }
         memset(&mic_data,0,sizeof(MIC_DATA_T));
         memcpy(mic_data.data, buf, sizeof(buf));
         mic_data.len = sizeof(buf);
---
+++ src/publish/main/continuous_read.h
@@ -0,0 +1,42 @@
+/**
+ * continuous_read.h
+ *
+ */
+
+#ifndef CONTINUOUS_READ_H_
+#define CONTINUOUS_READ_H_
+
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "esp_adc/adc_continuous.h"
+
+#define EXAMPLE_ADC_UNIT                    ADC_UNIT_1
+#define _EXAMPLE_ADC_UNIT_STR(unit)         #unit
+#define EXAMPLE_ADC_UNIT_STR(unit)          _EXAMPLE_ADC_UNIT_STR(unit)
+#define EXAMPLE_ADC_CONV_MODE               ADC_CONV_SINGLE_UNIT_1
+#define EXAMPLE_ADC_ATTEN                   ADC_ATTEN_DB_0
+#define EXAMPLE_ADC_BIT_WIDTH               SOC_ADC_DIGI_MAX_BITWIDTH
+
+#if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2
+#define EXAMPLE_ADC_OUTPUT_TYPE             ADC_DIGI_OUTPUT_FORMAT_TYPE1
+#define EXAMPLE_ADC_GET_CHANNEL(p_data)     ((p_data)->type1.channel)
+#define EXAMPLE_ADC_GET_DATA(p_data)        ((p_data)->type1.data)
+#else
+#define EXAMPLE_ADC_OUTPUT_TYPE             ADC_DIGI_OUTPUT_FORMAT_TYPE2
+#define EXAMPLE_ADC_GET_CHANNEL(p_data)     ((p_data)->type2.channel)
+#define EXAMPLE_ADC_GET_DATA(p_data)        ((p_data)->type2.data)
+#endif
+
+#define EXAMPLE_READ_LEN                    256
+
+
+extern void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num, adc_continuous_handle_t *out_handle);
+
+
+#endif /* CONTINUOUS_READ_H_ */
